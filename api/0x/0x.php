92aba7c37685cda52ca988bab1dc9c17bf0e9e4e
Denzg <guitiwdenver@gmail.com>
2026-03-18 23:02:23 +0800

    test update1

--- api/0x/0x.php
+++ api/0x/0x.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST');
-// header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   exit(0);
