0c92a40a47a62895116425989b0a38ccd09a5a4b
Denzg <guitiwdenver@gmail.com>
2026-03-19 13:45:59 +0800

    test update 6

---
+++ assets/transactions-CeTKmpDd.js
@@ -0,0 +1,16 @@
+import{cY as f,cV as a,cW as m}from"./index-tnyDZy3B.js";import{c as p}from"./index-TNN8dS9m.js";import"./index-CMC6hiAH.js";import"./index-K_aukfJi.js";import"./if-defined-BVydphS1.js";import"./index-yYEGSc0-.js";import"./index-XFU5X6DF.js";import"./index-CSMpw0hg.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
